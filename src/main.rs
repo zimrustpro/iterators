6c5baffdcb274fae3c6a5c395c32dd9aec525ccc
zimrustpro <zimrustpro@gmail.com>
2024-11-21 15:16:26 +0200

    quick example to show the 3 types of iterators

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let vector1 = vec![1, 2, 3];
+    let mut vector2 = vec![10, 20, 30];
+
+    for num in vector1.iter() {
+        println!("Printing a &i32: {}", num);
+    }
+
+    // for-loop --> iterator of values
+    // it owns the values, so vector1 nolonger exists after this forloop is done
+    for num in vector1 {
+        println!("Printing an i32: {}", num);
+    }
+
+    for num in vector2.iter_mut() {
+        *num *= 10;
+        println!("num is now {}", num);
+    }
+    println!("{:?}", vector2);
 }
