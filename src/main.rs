7aacdfe184a602bd86248328ab695d97647863e4
zimrustpro <zimrustpro@gmail.com>
2024-11-21 15:36:00 +0200

    Use `map()` and `for_each()` on iterators

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
 fn main() {
     let vector1 = vec![1, 2, 3];
+    let vector1_a = vector1.iter().map(|x| x + 1).collect::<Vec<i32>>();
+    let vector1_b = vector1.into_iter().map(|x| x * 10).collect::<Vec<i32>>();
+
     let mut vector2 = vec![10, 20, 30];
+    vector2.iter_mut().for_each(|x| *x += 100);
 
-    for num in vector1.iter() {
-        println!("Printing a &i32: {}", num);
-    }
-
-    // for-loop --> iterator of values
-    // it owns the values, so vector1 nolonger exists after this forloop is done
-    for num in vector1 {
-        println!("Printing an i32: {}", num);
-    }
-
-    for num in vector2.iter_mut() {
-        *num *= 10;
-        println!("num is now {}", num);
-    }
+    println!("{:?}", vector1_a);
+    println!("{:?}", vector1_b);
     println!("{:?}", vector2);
 }
