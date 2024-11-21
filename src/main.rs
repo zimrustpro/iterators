44195d593d11fd2cd288ba812ed466f8a59a1f12
zimrustpro <zimrustpro@gmail.com>
2024-11-21 15:38:55 +0200

    use iterator `.next()` method to return an Option

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 fn main() {
-    let vector1 = vec![1, 2, 3];
-    let vector1_a = vector1.iter().map(|x| x + 1).collect::<Vec<i32>>();
-    let vector1_b = vector1.into_iter().map(|x| x * 10).collect::<Vec<i32>>();
+    let my_vec = vec!['b', 'l', 'e', 's', 's', 'e', 'd'];
+    let mut my_vec_iter = my_vec.iter();
 
-    let mut vector2 = vec![10, 20, 30];
-    vector2.iter_mut().for_each(|x| *x += 100);
-
-    println!("{:?}", vector1_a);
-    println!("{:?}", vector1_b);
-    println!("{:?}", vector2);
+    assert_eq!(my_vec_iter.next(), Some(&'b'));
+    assert_eq!(my_vec_iter.next(), Some(&'l'));
+    assert_eq!(my_vec_iter.next(), Some(&'e'));
+    assert_eq!(my_vec_iter.next(), Some(&'s'));
+    assert_eq!(my_vec_iter.next(), Some(&'s'));
+    assert_eq!(my_vec_iter.next(), Some(&'e'));
+    assert_eq!(my_vec_iter.next(), Some(&'d'));
+    assert_eq!(my_vec_iter.next(), None);
+    assert_eq!(my_vec_iter.next(), None);
 }
